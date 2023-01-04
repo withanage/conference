af94eed203ee112bcc84e4b21116a55343c6e3b0
withanage <dulip.withanage@gmail.com>
2023-01-04 15:35:53 +0100

    Confident Backend Support

--- ConferencePlugin.inc.php
+++ ConferencePlugin.inc.php
@@ -3,9 +3,33 @@ import('lib.pkp.classes.plugins.GenericPlugin');
 class ConferencePlugin extends GenericPlugin {
 	public function register($category, $path, $mainContextId = NULL) {
 		$success = parent::register($category, $path, $mainContextId);
+		if ($success && $this->getEnabled($mainContextId)) {
+			HookRegistry::register('Templates::Editor::Issues::IssueData::AdditionalMetadata', array($this, 'metadataFieldEdit'));
+			HookRegistry::register('issueform::execute', array($this, 'metadataFieldExecute'));
+			HookRegistry::register('issuedao::getAdditionalFieldNames', array($this, 'handleAdditionalFieldNames'));
+
+		}
+
+		HookRegistry::register('Schema::get::issue', function ($hookName, $args) {
+			$schema = $args[0];
+
+			$schema->properties->conferenceDOI = (object)[
+				'type' => 'string',
+				'apiSummary' => true,
+				'validation' => ['nullable']
+			];
+		});
+
 		return $success;
 
 	}
+
+	function handleAdditionalFieldNames($hookName, $params) {
+		$fields =& $params[1];
+		$fields[] = 'conferenceDOI';
+		return false;
+	}
+
 
 	/**
 	 * Provide a name for this plugin
@@ -25,5 +49,33 @@ class ConferencePlugin extends GenericPlugin {
 	 */
 	public function getDescription() {
 		return '';
+	}
+
+	/**
+	 * Insert metadataEditForm
+	 *
+	 * @param $hookName
+	 * @param $params
+	 * @return false
+	 */
+	function metadataFieldEdit($hookName, $params): bool
+	{
+		$smarty =& $params[1];
+		$output =& $params[2];
+		$output .= $smarty->fetch($this->getTemplateResource('metadataForm.tpl'));
+		return false;
+	}
+
+	function metadataFieldExecute($hookName, $params)
+	{
+		$request = $this->getRequest();
+		$requestVars  = $request->getUserVars();
+		$conferenceDOI = $requestVars['conferenceDOI'];
+		$issue =& $params[0]->issue;
+		if ($issue && $conferenceDOI) {
+			$issue->setData('conferenceDOI', $conferenceDOI);
+		}
+
+
 	}
 }
